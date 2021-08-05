ded508b2d259aaa3be2d5ed319e7cc3f5eb0c3bc
Lommerde, Nisse - Student Rijn IJssel <00281107@rijnijssel.nl>
2021-08-05 15:23:04 +0200

    test

--- ProjectManagement/Experiments/rtmidi.py
+++ ProjectManagement/Experiments/rtmidi.py
@@ -1,24 +1,38 @@
-import rtmidi
+"""Show how to open an output port and send MIDI events."""
 
-midiin = rtmidi.RtMidiIn()
+from __future__ import print_function
 
-def print_message(midi):
-    if midi.isNoteOn():
-        print('ON: ', midi.getMidiNoteName(midi.getNoteNumber()), midi.getVelocity())
-    elif midi.isNoteOff():
-        print('OFF:', midi.getMidiNoteName(midi.getNoteNumber()))
-    elif midi.isController():
-        print('CONTROLLER', midi.getControllerNumber(), midi.getControllerValue())
+import logging
+import sys
+import time
 
-ports = range(midiin.getPortCount())
-if ports:
-    for i in ports:
-        print(midiin.getPortName(i))
-    print("Opening port 0!") 
-    midiin.openPort(0)
-    while True:
-        m = midiin.getMessage(250) # some timeout in ms
-        if m:
-            print_message(m)
-else:
-    print('NO MIDI INPUT PORTS!')
+from rtmidi.midiutil import open_midioutput
+from rtmidi.midiconstants import NOTE_OFF, NOTE_ON
+
+
+log = logging.getLogger('midiout')
+logging.basicConfig(level=logging.DEBUG)
+
+# Prompts user for MIDI input port, unless a valid port number or name
+# is given as the first argument on the command line.
+# API backend defaults to ALSA on Linux.
+port = sys.argv[1] if len(sys.argv) > 1 else None
+
+try:
+    midiout, port_name = open_midioutput(port)
+except (EOFError, KeyboardInterrupt):
+    sys.exit()
+
+note_on = [NOTE_ON, 60, 112]  # channel 1, middle C, velocity 112
+note_off = [NOTE_OFF, 60, 0]
+
+with midiout:
+    print("Sending NoteOn event.")
+    midiout.send_message(note_on)
+    time.sleep(1)
+    print("Sending NoteOff event.")
+    midiout.send_message(note_off)
+    time.sleep(0.1)
+
+del midiout
+print("Exit.")

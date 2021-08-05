b9f5517895bd86c6ff3fea014db09d88838320eb
Lommerde, Nisse - Student Rijn IJssel <00281107@rijnijssel.nl>
2021-08-05 17:31:09 +0200

    Pygame MIDI!!!!!

--- Modules/Midi.py
+++ Modules/Midi.py
@@ -1,31 +1,26 @@
-# MIDI interface
+# MIDI Interface
 
-import rtmidi, time
+import pygame.midi
+#import time
 
-class Midi:
+class MidiInterface:
+
   def __init__(self):
-    # rtmidi setup
-    
-    self.midiOut = rtmidi.MidiOut()
-    self.availablePorts = self.midiOut.get_ports()
-    
-    # Setup first available midi device if possible
-    if self.availablePorts:
-      self.midiOut.open_port(0)
-    else:
-      self.midiOut.open_virtual_port("RPiMIDISC Virtual Out")
+    pygame.midi.init()
 
-  def playNoteTest(self):
-    with self.midiOut:
-      note_on = [0x90, 60, 112] # channel 1, middle C, velocity 112
-      note_off = [0x80, 60, 0]
-      self.midiOut.send_message(note_on)
-      time.sleep(0.5)
-      self.midiOut.send_message(note_off)
-      time.sleep(0.1)
+    #print(pygame.midi.get_default_output_id())
+    #print(pygame.midi.get_device_info(2))
 
-  #del midiOut
+    player = pygame.midi.Output(0)
 
+    player.set_instrument(0)
 
+    print('Playing...')
 
-  
+player.note_on(64, 100)
+time.sleep(1)
+player.note_off(64)
+
+print('Played')
+
+pygame.midi.quit()

def03f64a462c3c78b2893e74bcd1104a80866fa
Lommerde, Nisse - Student Rijn IJssel <00281107@rijnijssel.nl>
2021-08-20 11:57:54 +0200

    display highest note layer on play (temporary)

--- Modules/Sequencer.py
+++ Modules/Sequencer.py
@@ -181,6 +181,12 @@ class Sequencer:
 
 	def sequencerStep(self):
 		# Executes each step
+
+		# Select topmost layer for displaying #FIXME: temporary, don't do this w multiple NCMs
+		for idx in range(self.noteLayerAmount):
+			if self.patterns[self.patternStep].patternSteps[self.seqstep].noteLayers[idx].note != 0:
+				self.patterns[self.patternStep].patternSteps[self.seqstep].selectedLayer[0] = idx
+
 		self.sendMidi(False) # Midi Output
 
 		# Checks if we should increase seqstep or roll back
